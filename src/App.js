90e8d8b79ef804d9e04b6ea39b96959922021800
chenxq <358009028@qq.com>
2019-05-20 23:32:29 +0800

    click button to change square/'s color

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ import Square from './Square';
 
 const array = [1, 2, 3];
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        color: 'red',
+    };
+  }
+
+  colorChange = () => {
+    let color = this.state.color === 'red' ? 'green' : 'red';
+    this.setState({
+      color: color
+    })
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={i} color={this.state.color}/>;
   }
   
   render() {
@@ -15,6 +29,9 @@ class App extends React.Component {
         <div>
           {array.map(item => this.renderSquare(item))}
         </div>
+        <button onClick={this.colorChange}>
+          Change Color
+        </button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- src/Square.js
+++ src/Square.js
@@ -1,36 +1,22 @@
 import React from 'react';
 
 class Square extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            times: 1,
-            bgColor: 'red',
+            color: this.props.color,
         };
     }
 
-    clickHandler = () => {
-        alert(this.props.value);
-    }
-
-    changeGreen = () => {
-        console.log('===> start change color')
-        // `setState` is asynchronous
-        this.setState({bgColor: 'green'}, () => {
-            console.log('===>set state done')
-        });
-        console.log('bg color', this.state.bgColor);
-    }
-
     render() {
-        console.log('===>', this.state);
-        const {times} = this.state;
+        console.log('===>', this.props);
+        // const {times} = this.state;
         return (
             <div>
                 <div
                     title="test"
-                    onClick={this.clickHandler}
-                    style={{ margin: 10, width: 100 * times, height: 100 * times, backgroundColor: this.state.bgColor }}
+                    // onClick={this.clickHandler}
+                    style={{ margin: 10, width: 100, height: 100, backgroundColor: this.props.color }}
                 />
             </div>
         )
